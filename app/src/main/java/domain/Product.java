5e06da4a2174328d7dfdda24656a29e8db9a76f0
Keylie Seth 0005 <keylie.seth0005@stud.hkr.se>
2026-05-06 17:11:02 +0200

    cleaned some code in Product class

--- app/src/main/java/domain/Product.java
+++ app/src/main/java/domain/Product.java
@@ -1,6 +1,38 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
-    
-    
+    private String name;
+    private String category;
+    private int estimatedLifespan;
+    private boolean recycled;
+
+    public Product(String name, String category, int estimatedLifespan) {
+        this.name = name;
+        this.category = category;
+        this.estimatedLifespan = estimatedLifespan;
+        this.recycled = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getEstimatedLifespan() {
+        return estimatedLifespan;
+    }
+
+    public boolean isRecycled() {
+        return recycled;
+    }
+
+    public void setRecycled(boolean recycled) {
+        this.recycled = recycled;
+    }
 }
